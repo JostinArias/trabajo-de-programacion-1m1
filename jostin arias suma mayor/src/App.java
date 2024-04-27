b1de3c74571e98e2658d62024a9ea21fce6f4a02
jostin arias <jostinalexariassolis@gmail.com>
2024-04-27 09:56:03 -0600

    primer cambio confirmado

---
+++ jostin arias suma mayor/src/App.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class App {
+    
+    //variables de un metodo  
+    public static int V1, V2, V3;
+    public static void main(String[] args) throws Exception {
+
+        //capturar informacion del teclado;
+        Scanner teclas = new Scanner(System.in);
+
+        //ciclo cuando el usuario escriba 2  o 3 numeros iguales se repetira hasta que los 3 numeros sean diferentes;
+        do {
+
+            try {
+
+                System.out.println("Ingrese tres numeros enteros diferentes");
+                V1 = teclas.nextInt();
+                V2 = teclas.nextInt();
+                V3 = teclas.nextInt();
+
+                //para excepciones
+            } catch (Exception str) {
+
+                System.out.println(str);
+                System.out.println("Ingrese valores correctos solo se aceptan valores int");
+                teclas.nextLine();
+            }
+            //hasta que esta condicion no se cumpla se finaliza el ciclo;
+        } while (V1 == V2 || V1 == V3 || V2 == V3);
+
+        
+    
+    
+        teclas.close();
+
+        //buscar numero mayor
+        int May = Math.max(Math.max(V1,V2),V3);
+
+        //sumar 2 numeros mas pequeños
+        int SumDMin = V1+V2+V3-Math.max
+        (Math.max(V1,V2),V3);
+
+        //diferencia de los dos mayores
+        int DifDeMay = Math.max(Math.max(V1,V2),V3)-Math.max
+        (Math.min(V1,V2), Math.min(Math.max(V1,V2),V3));
+
+        
+
+        //Mostrar la suma de menores, diferencia de mayores y mostrar el numero mayor
+        System.out.println("suma de los dos numeros menores: "+SumDMin);
+        System.out.println("resta de los 2 numeros mayores: "+DifDeMay);
+        System.out.println("numero mayor: "+May);
+        
+    }
+}
